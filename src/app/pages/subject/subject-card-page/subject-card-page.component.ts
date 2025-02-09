1ccea763900e41c16bd37c4b89e600ee2831015a
kimoleg200301 <kimoleg200301@gmail.com>
2025-02-09 23:16:41 +0500

    minor changes

---
+++ src/app/pages/subject/subject-card-page/subject-card-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-subject-card-page',
+  imports: [],
+  templateUrl: './subject-card-page.component.html',
+})
+export class SubjectCardPageComponent {
+
+}
