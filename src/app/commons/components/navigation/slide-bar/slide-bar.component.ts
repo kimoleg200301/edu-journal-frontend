692b9e2dd55547bad69958811bbd300ef63e0501
kimoleg_workstation <kimoleg200301@gmail.com>
2025-02-11 11:55:32 +0500

    added pages

---
+++ src/app/commons/components/navigation/slide-bar/slide-bar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-slide-bar',
+  imports: [],
+  templateUrl: './slide-bar.component.html',
+  standalone: true
+})
+export class SlideBarComponent {
+
+}
